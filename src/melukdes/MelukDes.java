137e282e0000c529b426fcd84bd9264bdd8e92e8
Juan <jgsierraa@unal.edu.co>
2017-11-01 00:29:14 -0500

    HUh

--- src/melukdes/MelukDes.java
+++ src/melukdes/MelukDes.java
@@ -24,35 +24,46 @@ public class MelukDes {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\Juan\\Documents\\NetBeansProjects\\MelukDes\\src\\melukdes\\file.txt")))) {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
-
+            
             while (line != null) {
                 sb.append(line);
                 sb.append(System.lineSeparator());
                 line = br.readLine();
             }
-
+            
             String everything = sb.toString();
-
+            
             br.close();
             return everything;
         } catch (Exception e) {
             return e.toString();
         }
-
+        
     }
-
+    
     public static void main(String[] args) {
         MelukDes prueba = new MelukDes();
         String[] dividido = prueba.leerTxt().split("\\s+");
-
+        int contadorDeMemoria = 00;
+        
         for (int i = 0; i < dividido.length; i++) {
-            //System.out.println(dividido[i]);
             
+            // Comando actual que se esta leyendo
+            String aux = dividido[i]; 
             
-            
-            
-            if (dividido[i].length() >= 5) {
-                String dude = dividido[i].substring(0, 4);
+            // si es un .code o .data
+            if (aux.startsWith(".")) {
+                
+                
+                
+               
+                
+             // esto es un número o valor NO CREO QUE LO NECESITEMOS   
+            } else if (aux.startsWith("0")) {
+                
+            } // aca es si es un loop + algun numero
+            else if (dividido[i].length() >= 5) {
+                
                 if (dividido[i].startsWith("Loop")) {
                     System.out.println("Aqui encontro algo dude");
                 }
@@ -60,11 +71,35 @@ public class MelukDes {
                     System.out.println("coge mayusculas?");
                 }
                 System.out.println(dude);
-
+                
+            } else {
+                
+                if (dividido[i].equalsIgnoreCase("ldi")) {
+                    
+                } else if (dividido[i].equalsIgnoreCase("ld")) {
+                    
+                } else if (dividido[i].equalsIgnoreCase("st")) {
+                    
+                } else if (dividido[i].equalsIgnoreCase("add")) {
+                    
+                } else if (dividido[i].equalsIgnoreCase("inc")) {
+                    
+                } else if (dividido[i].equalsIgnoreCase("neg")) {
+                    
+                } else if (dividido[i].equalsIgnoreCase("sub")) {
+                    
+                } else if (dividido[i].equalsIgnoreCase("jmp")) {
+                    
+                } else if (dividido[i].equalsIgnoreCase("jz")) {
+                    
+                } else if (dividido[i].equalsIgnoreCase("jn")) {
+                    
+                }
+                
             }
-
+            
         }
-
+        
     }
-
+    
 }
